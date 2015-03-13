ca6e83ad9aadc5d977cb0825c2847faca8676cd6
Wei Zhang <wz2313@imperial.ac.uk>
2015-03-13 05:59:44 +0000

    Renaming

--- tests/com/tfl/billing/TravelTrackerTests.java
+++ tests/com/tfl/billing/TravelTrackerTests.java
@@ -18,6 +18,6 @@ import cucumber.api.junit.Cucumber;
 		glue = "classpath:com/tfl/billing",
 		plugin = { "progress", "html:reports/cucumber" }
 )
-public class TravelTrackerBDDTests {
+public class TravelTrackerTests {
 	
 }
